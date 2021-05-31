9e1139900e0e34e8123565797ed585e7f0878c8b
msolorio <msolorio.github@gmail.com>
2021-05-30 18:00:55 -0700

    [wip] Adding state to mainPage component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,25 @@
+import React from 'react';
 import Sidebar from '../components/mainPageComponents/Sidebar';
 import CategoryDetail from '../components/mainPageComponents/CategoryDetail';
 import RecipesList from '../components/mainPageComponents/RecipesList';
 import categoryData from '../categoryData';
 
-function MainPage() {
-  return (
-    <div className="main-page">
-      <Sidebar categories={categoryData} />
-      <main className="category-info">
-        <CategoryDetail category={categoryData[0]} />
-        <RecipesList recipes={categoryData[0].recipes} />
-      </main>
-    </div>
-  );
+class MainPage extends React.Component {
+  state = {
+    categoryIndex: 1
+  }
+
+  render() {
+    return (
+      <div className="main-page">
+        <Sidebar categories={categoryData} />
+        <main className="category-info">
+          <CategoryDetail category={categoryData[this.state.categoryIndex]} />
+          <RecipesList recipes={categoryData[this.state.categoryIndex].recipes} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
